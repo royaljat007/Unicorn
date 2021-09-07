f18e68c23818c13366a3f71a4bfc3c9c48410932
webmadhusudan <madsud888@gmail.com>
2021-09-07 11:41:01 +0530

    homepageresponsiveandourculter

--- js/custom.js
+++ js/custom.js
@@ -9,8 +9,43 @@ $(document).ready(function () {
       },
     },
   });
+  $(".why-us-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
+  });
+  $(".brands-logo-carousel").owlCarousel({
+    loop: false,
+    margin: 22,
+    nav: true,
+    center:true,
+    responsive: {
+      0: {
+        items: 1.5,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
+  });
 });
 // Contact Toggle
 $(".contact-model-btn").click(function () {
   $(".contact-main").toggleClass("hidden");
 });
+
+
